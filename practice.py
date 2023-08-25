84064b82a733ce0e1cd387f7f82cd54f96e1cf89
John Wynne <202280249@psu.palawan.edu.ph>
2023-08-25 23:18:23 +0800

    practice

--- practice.py
+++ practice.py
@@ -1,26 +1,17 @@
 import math
 
 def triangle_area(a, b, c):
-    s = (a + b + c)/ 2
-    area = math.sqrt(s * (s - a) * (s -b ) * (s - c))
+    s = (a + b + c) / 2
+    area = math.sqrt(s * (s - a) * (s - b) * (s - c))
     return area
 
-
-a = 5
-b= 6
-c= 7
+a = float(input("Enter the length of side a: "))
+b = float(input("Enter the length of side b: "))
+c = float(input("Enter the length of side c: "))
 area = triangle_area(a, b, c)
 print("The area of the triangle is:", area)
 
-
-def is_leap_year(year):
-    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-        return True
-    else:
-        return False
-
-
-year = 2024
+year = int(input("Enter a year: "))
 if is_leap_year(year):
     print(f"{year} is a leap year.")
 else:
